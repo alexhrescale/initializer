4435c6650177893be21fde59681c2fb45d8643aa
Alex Huang <alexh@rescale.com>
2019-06-17 10:21:04 -0700

    add run count + timestamp into loop runner history

--- util.sh
+++ util.sh
@@ -5,6 +5,7 @@ looped-command-runner() {
     touch $RUNNER_FILE
     touch $HISTORY_FILE
     _oldhash=
+    _count=0
     while [ -e $RUNNER_FILE ]; do
         _newhash=$(md5sum $RUNNER_FILE)
         if [ "$_oldhash" != "$_newhash" ]; then
@@ -12,6 +13,8 @@ looped-command-runner() {
                 bash $RUNNER_FILE
             );
             if [ $? -eq 0 ]; then
+                _count=$(($_count + 1))
+                echo '# '$(printf "%03d" $_count)' '$(date +%F" "%T) >> $HISTORY_FILE
                 cat $RUNNER_FILE >> $HISTORY_FILE
                 truncate -s 0 $RUNNER_FILE
             fi
